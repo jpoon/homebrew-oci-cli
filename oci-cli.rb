fa89a0a7dbba407fba581eb452f384d3d5fb037d
Jason Poon <jason.the.poon@oracle.com>
2018-10-25 12:01:32 -0700

    fix: brew audit errors

--- oci-cli.rb
+++ oci-cli.rb
@@ -1,25 +1,25 @@
 class OciCli < Formula
-    include Language::Python::Virtualenv
+  include Language::Python::Virtualenv
 
-    desc "Oracle Cloud Infrastructure CLI"
-    homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
-    url "https://github.com/oracle/oci-cli/archive/v2.4.35.tar.gz"
-    sha256 "9fc24ce4a58abaf427edeb21a0198cd6c07b3fce115e801f7341750a8da54d8f"
-    head "https://github.com/oracle/oci-cli.git"
+  desc "Oracle Cloud Infrastructure CLI"
+  homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
+  url "https://github.com/oracle/oci-cli/archive/v2.4.35.tar.gz"
+  sha256 "9fc24ce4a58abaf427edeb21a0198cd6c07b3fce115e801f7341750a8da54d8f"
+  head "https://github.com/oracle/oci-cli.git"
 
-    depends_on "python"
+  depends_on "python"
 
-    def install
-      venv = virtualenv_create(libexec, "python3")
-      system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
                                 "--ignore-installed", buildpath
 
-      venv.pip_install_and_link buildpath
+    venv.pip_install_and_link buildpath
 
-      bin.install_symlink libexec/"bin/oci"
-    end
+    bin.install_symlink libexec/"bin/oci"
+  end
 
-    test do
-      assert_match "Oracle Cloud Infrastructure", shell_output("#{bin}/oci help")
-    end
+  test do
+    assert_match "Oracle Cloud Infrastructure", shell_output("#{bin}/oci help")
+  end
 end
