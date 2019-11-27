4abc2b960a251da9f8a45b8c6794404cae5dfb87
philchung <phil.chung@oracle.com>
2019-11-27 10:06:32 -0500

    v2.6.14

--- oci-cli.rb
+++ oci-cli.rb
@@ -3,8 +3,8 @@ class OciCli < Formula
 
   desc "Oracle Cloud Infrastructure CLI"
   homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
-  url "https://github.com/oracle/oci-cli/archive/v2.6.13.tar.gz"
-  sha256 "cbf8126a7fb2c77d1a0d65b638a4f60bf0b740342bdb863346ba0ff7661ed918"
+  url "https://github.com/oracle/oci-cli/archive/v2.6.14.tar.gz"
+  sha256 "2940c83fe58551d937d1a10c33350141104088896beb1a6253d21fa651e6b141"
   head "https://github.com/oracle/oci-cli.git"
 
   depends_on "python"
