eeac37735efdd47297b40cdc1c8be9d3680a87bd
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2020-05-06 18:42:24 -0400

    Correct checksum on tar file

--- oci-cli.rb
+++ oci-cli.rb
@@ -4,7 +4,7 @@ class OciCli < Formula
   desc "Oracle Cloud Infrastructure CLI"
   homepage "https://docs.cloud.oracle.com/iaas/Content/API/Concepts/cliconcepts.htm"
   url "https://github.com/oracle/oci-cli/archive/v2.10.2.tar.gz"
-  sha256 "94a6898293d07f84a98add34c4df900f8ec64a570292279f6d91c781d37fd305"
+  sha256 "2944c468a3ae75f8a8ad023b6c7dab8e27cd9ac2b11c8c5ba5d6b65eec12d089"
   head "https://github.com/oracle/oci-cli.git"
 
   depends_on "python"
